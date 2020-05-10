ae6a1545c4e23364379a2be3a622b9a66785438c
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-10 17:38:02 +0530

    Update entitySet.py

--- udapi/mongodb/entitySet.py
+++ udapi/mongodb/entitySet.py
@@ -5,3 +5,9 @@
 #   Created by FOSS-X UDAPI Desgin Team on 7/05/20.
 #   Copyright © 2020 FOSS-X. All rights reserved.
 #   
+
+
+from ..util import *
+
+from flask import Blueprint
+mod = Blueprint('entitySetMongodb', __name__)
