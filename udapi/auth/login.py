88531122cc7b436a2054fb8e1e4b20782d7d1dce
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-10 11:07:54 +0530

    Create login.py

---
+++ udapi/auth/login.py
@@ -0,0 +1,51 @@
+
+from flask import Flask, jsonify, request
+import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector import FieldType
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+from flask import Blueprint
+mod = Blueprint('login', __name__)
+
+
+@mod.route('/login', methods=['POST'])
+def login():
+    username = request.json['username']
+    password = request.json['password']
+    databaseName = 'udapiDB'
+
+    # Find if user exists in the user table of udapi DB
+    try:
+        cnx = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="password",
+            database=databaseName
+        )
+        mycursor = cnx.cursor()
+        sql = "SELECT * FROM udapiDB.users WHERE username='" + username + "';"
+        mycursor.execute(sql)
+        entities = mycursor.fetchall()
+        attributes = [desc[0] for desc in mycursor.description]
+        data = []
+        for entity in entities:
+            data.append(dict(zip(attributes, entity)))
+        cnx.close()
+    except mysql.connector.Error as err:
+        return jsonify(success=0, error_code=err.errno, message=err.msg)
+
+    # Username doesn't exists
+    if not data:
+        return jsonify(success=0, error_code=401, message="User doesn't exists.")
+
+    if check_password_hash(data[0]['password'], password):
+        token = jwt.encode({'username' : username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, SECRET_KEY)
+        return jsonify({'jwtToken' : token.decode('UTF-8')})
+
+    return jsonify(success=0, error_code=401, message="Invalid password.")
