13cb34f5fb8071f0b664e0ff864b0d0590c66c4b
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-10 17:08:41 +0530

    Create databases.py

---
+++ udapi/mysql/databases.py
@@ -0,0 +1,111 @@
+# 
+#   databases.py
+#   Start of dataabases.py
+#
+#   Created by FOSS-X UDAPI Desgin Team on 7/05/20.
+#   Copyright © 2020 FOSS-X. All rights reserved.
+#   
+
+from flask import Flask, jsonify, request
+import mysql.connector
+from mysql.connector import errorcode
+from mysql.connector import FieldType
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+from ..util import *
+
+from flask import Blueprint
+mod = Blueprint('databasesMysql', __name__)
+
+
+@mod.route('/databases', methods=['GET'])
+@token_required
+def get_mysql_db(username):
+    """ List all the databases of databaseType = mysql """
+    databaseType = 'mysql'
+    try:
+        cnx = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="password",
+            database='udapiDB'
+        )
+        mycursor = cnx.cursor()
+        sql = "SELECT * FROM udapiDB.configs WHERE (username='" + username + "') AND (databaseType='" + databaseType + "');"
+        mycursor.execute(sql)
+        entities = mycursor.fetchall()
+        attributes = [desc[0] for desc in mycursor.description]
+        fieldType = [FieldType.get_info(desc[1]) for desc in mycursor.description]  # Debug code
+        results = []
+        for entity in entities:
+            results.append(entity[1])
+        cnx.close()
+        return jsonify(success=1, databases=results)
+
+    except mysql.connector.Error as err:
+        return jsonify(success=0, error_code=err.errno, message=err.msg)
+
+
+# Create the Database if it doesn't exist
+@mod.route('/databases', methods=['POST'])
+@token_required
+def create_mysql_db(username):
+    databaseName = request.json['databaseName']
+    password = get_password(username)
+
+    try:
+        cnx = mysql.connector.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            database=username + "_" + databaseName
+        )
+        cnx.close()
+        return jsonify(success=0, message="database Already Exists", error_code=401)
+
+    except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_BAD_DB_ERROR:
+                cnx = mysql.connector.connect(
+                    host="localhost",
+                    user=username,
+                    passwd=password
+                )
+                mycursor = cnx.cursor()
+                sql = "CREATE DATABASE " + username + "_" + databaseName + ";"
+                mycursor.execute(sql)
+                cnx.commit()
+
+                update_configs_add(username, databaseName, 'mysql')
+                return jsonify(success=1, message="Database created successfully.")
+            else:
+                return jsonify(success=0, error_code=err.errno, message=err.msg)
+
+
+@mod.route('/databases/<databaseName>', methods=["PUT"])
+@token_required
+def rename_mysql_db(username, databaseName):
+    """ This Functionallity is not yet available. """
+    return jsonify(success=0, message="Renaming the database name is not possible for MySql at the moment.")
+
+
+@mod.route('/databases/<databaseName>', methods=['DELETE'])
+@token_required
+def delete_mysql_db(username, databaseName):
+    password = get_password(username)
+    try:
+        cnx = mysql.connector.connect(
+            host="localhost",
+            user=username,
+            passwd=password
+        )
+        mycursor = cnx.cursor()
+        sql = "DROP DATABASE " + username + "_" + databaseName + ";"
+        mycursor.execute(sql)
+        cnx.close()
+        update_configs_remove(username, databaseName)
+        return jsonify(success=1, message="Database: " + databaseName + " deleted successfully.")
+
+    except mysql.connector.Error as err:
+        return jsonify(success=0, error_code=err.errno, message=err.msg)
