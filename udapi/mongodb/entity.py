293d405085d605d8bcc74608906e527ebe185b1e
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-10 17:37:59 +0530

    Update entity.py

---
+++ udapi/mongodb/entity.py
@@ -0,0 +1,12 @@
+# 
+#   entity.py
+#   Start of entity.py
+#
+#   Created by FOSS-X UDAPI Desgin Team on 7/05/20.
+#   Copyright © 2020 FOSS-X. All rights reserved.
+#   
+
+from ..util import *
+
+from flask import Blueprint
+mod = Blueprint('entityMongodb', __name__)
