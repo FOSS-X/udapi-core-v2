ad92ea788832efd5885a6a7d2bb7bc2e83236326
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-11 23:38:36 +0530

    Update databases.py

--- udapi/allDatabases/databases.py
+++ udapi/allDatabases/databases.py
@@ -9,12 +9,8 @@
 from flask import Flask, jsonify, request
 import mysql.connector
 from mysql.connector import errorcode
-from mysql.connector import FieldType
-from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
-import datetime
-from functools import wraps
 from ..util import *
+from ..util_mysql import *
 
 from flask import Blueprint
 mod = Blueprint('databasesAll', __name__)
@@ -25,23 +21,21 @@ mod = Blueprint('databasesAll', __name__)
 def get_mysql_db(username):
     """ List all the databases of all the databaseType """
     try:
-        cnx = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            passwd="password",
-            database='udapiDB'
-        )
+        cnx = connectSQLServerDB('root', 'password', 'udapiDB')
         mycursor = cnx.cursor()
-        sql = "SELECT * FROM udapiDB.configs WHERE (username='" + username + "');"
+        sql = "SELECT databaseName, databaseType FROM udapiDB.configs WHERE (username='" + username + "');"
         mycursor.execute(sql)
         entities = mycursor.fetchall()
         attributes = [desc[0] for desc in mycursor.description]
-        fieldType = [FieldType.get_info(desc[1]) for desc in mycursor.description]  # Debug code
-        results = []
+        dbs_mysql = []
+        dbs_mongodb = []
         for entity in entities:
-            results.append(entity[1])
+            if entity[1] == 'mysql':
+                dbs_mysql.append(entity[0])
+            if entity[1] == 'mongodb':
+                dbs_mongodb.append(entity[0])
         cnx.close()
-        return jsonify(success=1, databases=results)
+        return jsonify(success=1, mongodb=dbs_mongodb, mysql=dbs_mysql)
 
     except mysql.connector.Error as err:
         return jsonify(success=0, error_code=err.errno, message=err.msg)
