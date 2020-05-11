c69c61eb970b373df8ecb10780f976b2abba05e6
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2020-05-11 18:11:40 +0530

    bug fix

--- udapi/mongodb/error_handlers.py
+++ udapi/mongodb/error_handlers.py
@@ -4,17 +4,18 @@ from .mongoUtils import *
 
 mod = Blueprint('error_handlers','__name__')
 
-@mod.app_errorhandler(HTTPException)
-def handle_exception(e):
-    response = e.get_response()
-    response.data = jsonify({
-        "error_code": e.code,
-        "description": e.description,
-        "success": 0
-    })
-    return response
+# @mod.app_errorhandler(HTTPException)
+# def handle_exception(e):
+#     response = e.get_response()
+#     response.data = jsonify({
+#         "error_code": e.code,
+#         "description": e.description,
+#         "success": 0
+#     })
+#     return response
 
 @mod.app_errorhandler(mongoCustomException)
 def handle_duplicate_resource(error):
     response = error.toJson()
     return response
+
